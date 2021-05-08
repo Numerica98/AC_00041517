c2399c9297ff4840b4d5ff9f04c3701cc363d923
Numerica98 <00041517@uca.edu.sv>
2021-05-08 00:30:28 -0600

    Ejercicio 2 terminado

--- Tarea Laboratorio 3/ejercicio2.asm
+++ Tarea Laboratorio 3/ejercicio2.asm
@@ -1,1 +1,19 @@
-;Obtener el factorial del numero 5 (5! = 120 -> 78 en hexadecimal) y guardar el resultado en 20Bh
+org 100h
+
+section .text
+        XOR AX, AX
+        MOV AX, 1d
+        MOV word CX, 5d
+        CMP CX, 0d
+        JE guardar
+        JMP factorial
+
+; Obtencion del factorial
+factorial:
+        MUL CX
+        LOOP factorial
+guardar:
+        ; Guardando el resultado en el registro 20Bh
+        MOV word [20Bh], AX         
+exit:
+        int 20h
